02c2db2a2b71a08e6dc9a77409e22b1213c45eaf
lwwdsb <weiwen@usc.edu>
2025-10-14 20:30:31 -0700

    Add one of the skill:When press Enter,all the freezable stuff will be frozen.

---
+++ Assets/Scripts/Freeze/GlobalFreezeTrigger.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class GlobalFreezeTrigger : MonoBehaviour
+{
+    [Header("Freeze time in seconds")]
+    public float freezeDuration = 3f;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            TriggerFreeze();
+        }
+    }
+
+    void TriggerFreeze()
+    {
+        var allFreezables = FindObjectsOfType<Freezable>();
+
+        foreach (var f in allFreezables)
+        {
+            f.Freeze(freezeDuration);
+        }
+
+        Debug.Log($"[GlobalFreezeTrigger] All Freezable has been frozen for {freezeDuration} seconds");
+    }
+}
