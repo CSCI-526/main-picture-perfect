021884d551ece1ebc9d08231e1bbf8931df0689d
lwwdsb <weiwen@usc.edu>
2025-10-19 13:34:49 -0700

    add the MenuScene,setting and quit functions are not implemented yet

---
+++ Assets/Scripts/Menu/MainMenuUI.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuUI : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadScene("MainScene"); 
+    }
+
+    public void OpenSettings()
+    {
+        Debug.Log("Settings button clicked (not implemented)");
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quit button clicked");
+    }
+}
